5273af455619441536df85ce4cd9820a1d0c7378
ShaikmohammadDanish <shaikhmohammad.danish@quantela.com>
2023-07-02 23:21:35 +0530

    some change

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ const PORT = (process.env.PORT || 5000) as number;
 
 // health check
 fastify.get("/ping", async () => "pong\n");
+
 fastify.register(cors, { origin: "*" });
 fastify.register(userRoutes, { prefix: "/users" });
 
